615dab73f8a788e628e9818e5f6327d74773e58e
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-10-12 19:26:33 +0100

    edited the about me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,13 +6,13 @@ const About = () => {
 
     return (
         <div>
-        <h1 className="pageTitle">About me</h1>
+            <h1 className="pageTitle">About me</h1>
             <div className="aboutPageContents">
                 <img className="zviImg" src={zviImg} alt="me" />
-                <p> Hi, I am an aspiring junior software developer based in Manchester, UK. After working for 3 &frac12; years as a sales assistant I decided it was time to change my career path. So I left and joined <a href="https://wearecodenation.com/" className="codenationLink">Codenation</a> to begin the process of becoming a junior developer.</p>
-                <p>I started with the Software: Develop 3 week course and learned the basics of HTML, CSS and Python. You can check out the in-teminal RPG that I created with the rest of my cohort in the <Link className="codenationLink" to="/projects">projects</Link> page.</p> 
-                <p>After that course finished I signed up for the Master: Software 12 week course in which I learnt HTML and CSS amore in depth, then moved on to Javascript, REACT and interacting with API's which I really enjoyed. This took me to the end of the front-end part of the course. The back-end part consisted of Express.js, MongDB, Mongoose, SQL,Sequelize and linking up the front to the Back-end. Although slightly different I really liked this part as well.</p>  
-                <p>The course culminated with a 2 week group project which was really fun.  All the projects that I mad/was involved in can be found in the <Link className="codenationLink" to="/projects">projects</Link> page. </p>
+                <p> Hi, I am an aspiring junior software developer based in Manchester, UK. After working for 3 &frac12; years as a sales assistant I decided it was time to change my career path. So I left and joined <a href="https://wearecodenation.com/" className="codenationLink">Codenation</a> to begin the process of becoming a junior developer.</p><br/> 
+                <p>I started with the Software: Develop 3 week course and learned the basics of HTML, CSS and Python. You can check out the in-teminal RPG that I created with the rest of my cohort in the <Link className="codenationLink" to="/projects">projects</Link> page.</p><br/> 
+                <p>After that course finished I signed up for the Master: Software 12 week course in which I learnt HTML and CSS amore in depth, then moved on to Javascript, REACT and interacting with API's which I really enjoyed. This took me to the end of the front-end part of the course. The back-end part consisted of Express.js, MongDB, Mongoose, SQL,Sequelize and linking up the front to the Back-end. Although slightly different I really liked this part as well.</p>  <br/>
+                <p>The course culminated with a 2 week group project which was really fun.  All the projects that I made/was involved in can be found in the <Link className="codenationLink" to="/projects">projects</Link> page. </p><br/> <br/> 
             </div>
         </div>
     )
