90472e86dcbbf559922b8052cff37f7337b46182
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-10-12 15:58:26 +0100

    Added hamburger menu, about me temp desc, and small fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
 import { Link} from "react-router-dom";
+import menuIcon from "../Resorces/menu-burger-icon.svg";
+import { useState } from "react";
+import Modal from 'react-modal';
 
 
 function Navbar() {
-  
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+    const openCloseMenu = () => {
+      if (!menuOpen) {
+        setMenuOpen(true)
+        openModal()
+      } else {
+        setMenuOpen(false)
+        closeModal()
+      }
+    }
+
+    function openModal() {
+      setIsOpen(true);
+    }
+
+    function closeModal() {
+      setIsOpen(false);
+    }
+    
     return (
       <nav className="navWrap">
+        <img className="menuIcon" src={menuIcon} alt="menu" onClick={openCloseMenu}/>
+
         <Link className="navBarOptionText" to="/">Home</Link>
 
         <Link className="navBarOptionText" to="/about">About Me</Link>
@@ -12,6 +37,21 @@ function Navbar() {
         <Link className="navBarOptionText" to="/projects">Projects</Link>
 
         <Link className="navBarOptionText" to="/contact">Contact</Link>
+        <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className="menuModal"
+        overlayClassName="modalOverlay"
+        >
+          <Link onClick={closeModal} className="menuNavBarOptionText" to="/">Home</Link>
+
+          <Link onClick={closeModal} className="menuNavBarOptionText" to="/about">About Me</Link>
+
+          <Link onClick={closeModal} className="menuNavBarOptionText" to="/projects">Projects</Link>
+
+          <Link onClick={closeModal} className="menuNavBarOptionText" to="/contact">Contact</Link>
+        </Modal>
       </nav>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div>
         <h1 className="pageTitle">Welcome</h1>
-        <p className="pageDesc">This website is currently still being developed, however feel free to look around at what has already been built.</p>
+        <p className="pageDesc">This website is currently still under development, however feel free to look around at what has already been built.</p>
         <img className="constructionImg" src={construction} alt="under construction" />
 
         </div>
